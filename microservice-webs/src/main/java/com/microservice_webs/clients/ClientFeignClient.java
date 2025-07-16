340c6bb16de6ed02f78f6ac353937af0599959f5
MIKAELGG <mikael2g3g@gmail.com>
2025-07-16 13:02:30 -0500

    Production

--- microservice-webs/src/main/java/com/microservice_webs/clients/ClientFeignClient.java
+++ microservice-webs/src/main/java/com/microservice_webs/clients/ClientFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-client", url = "https://microservice-client.up.railway.app/api/clients")
+@FeignClient(name = "microservice-client", path = "/api/clients")
 public interface ClientFeignClient {
 
     @GetMapping
